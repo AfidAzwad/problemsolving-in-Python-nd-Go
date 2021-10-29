e3fa191511b3b2fdc1c3d0f76faa016fb00cd42c
Azwad22 <mdazwad22@gmail.com>
2021-10-30 00:11:31 +0600

    drawing book

--- Drawing Book/drawingbook.py
+++ Drawing Book/drawingbook.py
@@ -3,7 +3,15 @@ import math
 
 def countinTurn(n, p):
 
-    if (n-p) == n/p or (n-p) > n/2:
+    if n%2 != 0 and n-p == 1: # to avoid extra calculation and value flooring problem [input n=9, p=8]
+
+        return 0
+
+    elif n%2 == 0 and n-p == 1 and n != 2: # to avoid flooring problem and 2 pages book => input n=2, p=1
+
+        return 1
+    
+    elif (n-p) == n/p or (n-p) > n/2:
 
         return math.floor(p/2)
 
