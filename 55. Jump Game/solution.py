a3b201f2b4232428a4a23707c91e00b1ee0f6eac
Azwad-bs23 <mdazwad22@gmail.com>
2025-01-26 18:47:48 +0600

    [ADD] 55. Jump Game

---
+++ 55. Jump Game/solution.py
@@ -0,0 +1,20 @@
+def canJump(nums):
+    goal = len(nums) - 1
+
+    for i in range(goal, -1, -1):
+        if i + nums[i] >= goal:
+            goal = i
+    return True if goal == 0 else False
+
+# nums = [2,3,1,1,4]
+# nums = [0]
+nums = [2,5,0,0]
+print(canJump(nums))
+
+"""
+    Time Complexity (TC): O(n)
+    - We iterate through the array once in reverse order, so the complexity is linear with respect to the size of the array.
+
+    Space Complexity (SC): O(1)
+    - No additional data structures are used; only a few variables are maintained.
+"""
