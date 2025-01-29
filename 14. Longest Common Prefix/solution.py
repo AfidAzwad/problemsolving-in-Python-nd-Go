5385c5e9b0888247ee8f65c4bb559da1f8d38c39
Azwad-bs23 <mdazwad22@gmail.com>
2025-01-29 15:53:19 +0600

    [ADD] 14. Longest Common Prefix

---
+++ 14. Longest Common Prefix/solution.py
@@ -0,0 +1,12 @@
+def longestCommonPrefix(strs):
+    prefix = ''
+    for i in range(len(strs[0])):
+        for word in strs:
+            if i == len(word) or word[i] != strs[0][i]:
+                return prefix
+        prefix += word[i]
+    return prefix
+
+
+strs = ["flower","flow","flight"]
+print(longestCommonPrefix(strs))
