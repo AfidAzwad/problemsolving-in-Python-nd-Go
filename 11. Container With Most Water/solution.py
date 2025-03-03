d2ba7f140618d0d6d46ebd0d0e4944f2ad90a6c5
Azwad <mdazwad22@gmail.com>
2025-03-03 22:29:48 +0600

    [UPDATE] 11. Container With Most Water

--- 11. Container With Most Water/solution.py
+++ 11. Container With Most Water/solution.py
@@ -1,16 +1,14 @@
 def maxArea(height):
-    trap = 0
+    max_area = 0
     L, R = 0, len(height)-1
 
     while L < R:
-        trap = max(trap, (R - L) * min(height[L], height[R]))
-        if height[L] > height[R]:
-            R -= 1
-        elif height[L] < height[R]:
+        max_area = max(max_area, (R - L) * min(height[L], height[R]))
+        if height[L] < height[R]:
             L += 1
         else:
             R -= 1
-    return trap
+    return max_area
 
 height = [1,2,1]
 print(maxArea(height))
